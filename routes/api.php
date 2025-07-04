b9a95e2ea62214298a9fab51867ce0ad4ff60561
ZED-Magdy <zedmagdy@gmail.com>
2025-07-04 22:39:01 +0300

    feature(docs): Add openapi docs to document our apis

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,10 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->get('/user', fn (Request $request) => $request->user());
+Route::middleware('auth:sanctum')->get('/user', fn (Request $request) => new UserResource($request->user()));
 
 Route::post('/login', [AuthController::class, 'login']);

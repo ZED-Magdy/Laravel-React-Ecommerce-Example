4d01707845c1efeaa50fb41511662db720d764a5
ZED-Magdy <zedmagdy@gmail.com>
2025-07-06 05:33:50 +0300

    chore(update): replace favicon and modify page title in welcome view for branding consistency

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel + React + TypeScript</title>
+    <title>Daftra Ecommerce</title>
     @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/main.tsx'])
 </head>
